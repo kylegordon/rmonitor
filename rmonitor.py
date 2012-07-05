62e724c949c4db026e012e37a615e571c7060ca5
Kyle Gordon <kyle@lodge.glasgownet.com>
2012-07-05 18:00:07 +0100

    Initial checkin of a copy of Kevs linereciever

---
+++ rmonitor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+## LineReceiver network client
+## Courtesy of Kevin McDermott
+
+#!/usr/bin/env python
+
+from sys import stdout
+
+from twisted.internet.protocol import ReconnectingClientFactory
+from twisted.protocols.basic import LineOnlyReceiver
+from twisted.internet import reactor
+
+
+class RaceTimeReceiver(LineOnlyReceiver):
+    delimiter = "\n"
+
+    def lineReceived(self, data):
+        # Process the line you're receiving here...
+        print "Line received %s" % data
+
+
+class RaceTimeClientFactory(ReconnectingClientFactory):
+
+    protocol = RaceTimeReceiver
+
+    def buildProtocol(self, addr):
+        self.resetDelay()
+        return ReconnectingClientFactory.buildProtocol(self, addr)
+
+    def clientConnectionLost(self, connector, reason):
+        print "Lost connection.  Reason:", reason
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        print "Connection failed. Reason:", reason
+        ReconnectingClientFactory.clientConnectionFailed(self, connector,
+                                                         reason)
+if __name__ == "__main__":
+    factory = RaceTimeClientFactory()
+    reactor.connectTCP("localhost", 50000, factory)
+    reactor.run()
