09c7fec54390a3d922fa30b1b5418d5966fffa61
Kyle Gordon <kyle@lodge.glasgownet.com>
2012-07-10 22:42:08 +0100

    Sort of more worky stuff

--- rmonitor.py
+++ rmonitor.py
@@ -12,15 +12,17 @@ from twisted.internet import reactor
 
 #Initialize a 2D array
 
-height = 10
-width = 8
-competitors = [[0 for _ in xrange(height)] for _ in xrange(width)]
+#height = 10
+#width = 8
+#competitors = [[0 for _ in xrange(height)] for _ in xrange(width)]
+competitors = []
 # competitors = [['Transponder', 'Registration', 'First Name', 'Second name', 'Position', 'Last lap time', 'Best lap time', 'Best lap']]
 
 class RaceTimeReceiver(LineOnlyReceiver):
     delimiter = "\n"
 
     def lineReceived(self, data):
+    	competitor = []
         # Process the line you're receiving here...
         ## print "Line received %s" % data
         # Strip the carriage return and split it on commas
@@ -34,12 +36,11 @@ class RaceTimeReceiver(LineOnlyReceiver):
         if command == "$A":
                 # $A always comes before $COMP, and $A carries the transponder number
                 print "Competitor information : " + str(data)
-		x = 0 # This will become the row number for the competitor that we're updating.
-		competitors[x][0] = data[3] # Transponder
-		competitors[x][1] = data[1] # Number
-		competitors[x][2] = data[4] # first name
-		competitors[x][3] = data[5] # Second name
-		print competitors
+		competitor.append(data[3]) # Transponder
+		competitor.append(data[1]) # Number
+		competitor.append(data[4]) # first name
+		competitor.append(data[5]) # Second name
+		print competitor
         elif command == "$COMP":
                 print "Competitor information : " + str(data)
         elif command == "$B":
@@ -57,6 +58,13 @@ class RaceTimeReceiver(LineOnlyReceiver):
         elif command == "$J":
 		# Registration, lap time, Total time
                 print "Passing information : "  + str(data)
+		
+	## Append to the big array of doom here
+	# Check that competitor isn't empty
+	#if competitor.length:
+	#	competitors.append(competitor)
+	
+	## Call something to do something
 
 class RaceTimeClientFactory(ReconnectingClientFactory):
 
@@ -76,5 +84,5 @@ class RaceTimeClientFactory(ReconnectingClientFactory):
                                                          reason)
 if __name__ == "__main__":
     factory = RaceTimeClientFactory()
-    reactor.connectTCP("192.168.10.27", 50000, factory)
+    reactor.connectTCP("localhost", 50000, factory)
     reactor.run()
